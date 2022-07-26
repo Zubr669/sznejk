994a0bbd21921ab0f458d0dcac2a6b1e14207d7d
Piotr <zubr669@gmail.com>
2022-07-26 12:27:45 +0200

    added hearts

--- script.js
+++ script.js
@@ -14,10 +14,55 @@ function ramka() {
     ctx.lineTo(1, 1);
     ctx.stroke();
 };
-function kwadrat(x, y) {
-    ctx.fillRect(50 * x + 5, 50 * y + 5, 40, 40);
-}
 
+function drawHeart(fromx, fromy, tox, toy,lw,hlen,color) {
+
+    var x = fromx;
+    var y = fromy;
+    var width = lw ;
+    var height = hlen;
+  
+    ctx.save();
+    ctx.beginPath();
+    var topCurveHeight = height * 0.3;
+    ctx.moveTo(x, y + topCurveHeight);
+    // top left curve
+    ctx.bezierCurveTo(
+      x, y, 
+      x - width / 2, y, 
+      x - width / 2, y + topCurveHeight
+    );
+  
+    // bottom left curve
+    ctx.bezierCurveTo(
+      x - width / 2, y + (height + topCurveHeight) / 2, 
+      x, y + (height + topCurveHeight) / 2, 
+      x, y + height
+    );
+  
+    // bottom right curve
+    ctx.bezierCurveTo(
+      x, y + (height + topCurveHeight) / 2, 
+      x + width / 2, y + (height + topCurveHeight) / 2, 
+      x + width / 2, y + topCurveHeight
+    );
+  
+    // top right curve
+    ctx.bezierCurveTo(
+      x + width / 2, y, 
+      x, y, 
+      x, y + topCurveHeight
+    );
+  
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.restore();
+  
+  }
+function kwadrat(x, y) {
+    drawHeart(50 * x + 25, 50 * y +5 ,40,40, 40, 40,ctx.color);
+}
 
 var kierunek = 1;
 
